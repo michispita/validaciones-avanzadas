cec7cd921e315e368f320fde058d48ee3811001a
Julieta <julietadiazh02@gmail.com>
2024-10-10 20:19:50 -0300

    terminos en rojo y con mensaje

--- js/validacion.js
+++ js/validacion.js
@@ -17,3 +17,35 @@
       }, false)
     })
   })()
+
+
+// obtener el formulario y el checkbox
+const modal = document.getElementById('modalTerminos');
+const checkbox = document.getElementById('cbterminos');
+const form = document.getElementById('form')
+const btnTerminos = document.getElementById('btnTerminos');
+
+// validar términos y condiciones
+function validateTerms() {
+  if (!checkbox.checked) {
+    checkbox.setCustomValidity(btnTerminos.classList.add('is-invalid'));
+    btnTerminos.style.color = "red";
+    document.getElementById("mdlterminos").style.color = "red";
+    document.getElementById("terminos").style.color = "red";
+  } else {
+    checkbox.setCustomValidity("");
+  }
+}
+
+form.addEventListener('submit', function (event) {
+  validateTerms();
+
+  // prevenir el envío si no es valido
+  if (!form.checkValidity()) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  // Añadir clase 'was-validated' al formulario para activar estilos de Bootstrap
+  form.classList.add('was-validated');
+});
