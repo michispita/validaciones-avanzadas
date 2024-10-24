7ce64f6cc0a1327f2ce81fc35b7146590663d38e
priscillafm <priscillaferreri@gmail.com>
2024-10-24 18:25:02 -0300

    1

--- js/validacion.js
+++ js/validacion.js
@@ -1,19 +1,31 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+const form = document.getElementById('miFormulario');
+const password1 = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', function(event) {
+  // Validar si las contraseñas coinciden
+  if (password1.value !== password2.value) {
+    alert("Las contraseñas no coinciden");
+    event.preventDefault();  // Evitar que el formulario se envíe
+  }
+});
